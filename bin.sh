f4998b8b0a7f97d99671f1167edc8a2a60fc6408
LuYi <luyi985@gmail.com>
2018-06-10 20:44:35 +1000

    s3 sync

--- bin.sh
+++ bin.sh
@@ -2,17 +2,22 @@ BUCKET_NAME="lyi-bucket"
 TEMP_URL="http://${BUCKET_NAME}.s3.amazonaws.com/template/template.json"
 
 create_temp_s3() {
-    aws s3api create-bucket --acl private \
-                            --bucket ${BUCKET_NAME} \
-                            --create-bucket-configuration LocationConstraint=ap-southeast-2
+    aws s3api create-bucket \
+    --acl private \
+    --bucket ${BUCKET_NAME} \
+    --create-bucket-configuration LocationConstraint=ap-southeast-2
 }
 
 delete_temp_s3() {
-    aws s3 rb s3://${BUCKET_NAME} --force
+    aws s3 rb s3://${BUCKET_NAME} \
+    --force
 }
 
-push_temp() {
-    aws s3 cp \
-    template.json s3://${BUCKET_NAME}/template/ \
+sync_folder() {
+    aws s3 sync ./files s3://${BUCKET_NAME}/files \
+    --delete \
+    --acl public-read && \
+    aws s3 sync ./cf s3://${BUCKET_NAME}/cf \
+    --delete \
     --acl public-read
 }
