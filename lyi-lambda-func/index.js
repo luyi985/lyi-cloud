4a038b9e616f85caf2b371d9015f9d957e28f90b
LuYi <luyi985@gmail.com>
2018-06-19 21:13:06 +1000

    lambda function

--- lyi-lambda-func/index.js
+++ lyi-lambda-func/index.js
@@ -1,5 +1,34 @@
-exports.handler = (event, context, callback) => {
-    console.log("lambda function test");
-    console.log(event);
-    console.log(context);
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+const s3Wrapper = (params, op) => new Promise(resolve => {
+    if( typeof op !== "string" || !!!op || !!!op.length)
+    throw new Error('Not proper s3 method');
+
+    s3[op](params, (err, data) => {
+        if (err) throw new Error(err);
+        resolve(data);
+    })
+})
+
+async function play(event, context, callback){
+    try {
+        const fileObj = await s3Wrapper({
+            Bucket: "lyi-bucket",
+            Prefix: "files/"
+        }, 'listObjectsV2');
+
+        const img = await s3Wrapper({
+            Bucket: "lyi-bucket",
+            Key: "files/little-kitty.jpeg"
+        }, 'getObject');
+
+        console.log('----img-----', img)
+        console.log('----fileObj------', fileObj)
+    } catch(e) {
+        console.log("e")
+    }
 }
+
+play()
+
+exports.handler = play;
